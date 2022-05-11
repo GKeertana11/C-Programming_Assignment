a21c6401d89558b3ff92ef0dcef6f873a28cb9a3
KeertanaG <keertana.govte@yesgnome.com>
2022-05-11 11:34:34 +0530

    Chapter2 done.

---
+++ Assets/Chapter2/ConstantVariableDeclarations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVariableDeclarations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 10;//Variable declaration.
+            const int months = 12;//constant declaration.
+            Console.WriteLine(health);
+            Console.WriteLine(months);//executable statements.
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assets/Chapter2/HelloWorldWithUnity.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorldWithUnity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+        Debug.Log("I am keertana");
+        Debug.Log("How are u?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
