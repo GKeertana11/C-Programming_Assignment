74ecbdb38c9d6201a07f2147ef37614ec2e6eab5
KeertanaG <keertana.govte@yesgnome.com>
2022-05-11 11:20:49 +0530

    Chapter 1 done.

---
+++ Assets/Chapter1/Chapter1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    
+        internal class Program
+        {
+            
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, world");
+                Console.WriteLine("I am keertana");
+                Console.WriteLine("How are u?");
+                Console.WriteLine();
+            Console.ReadLine();
+            }
+        }
+    }
