65e2ea1401a11b6b08591e0b47db3c24bf6a44e2
KeertanaG <keertana.govte@yesgnome.com>
2022-05-12 12:26:27 +0530

    Datatypes, variables and constants-Chapter 3done. Chapter4- Classes and objects learnt using circle example.

---
+++ Assets/Chapter3/DataTypesAndvariables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesAndvariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 100;
+            float pie = 3.14f;
+            bool istrue = true;
+            char letter = 'A';
+            string name = "Keertana";
+            Console.WriteLine(amount);
+            Console.WriteLine(pie);
+            Console.WriteLine(istrue);
+            Console.WriteLine(letter);
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
