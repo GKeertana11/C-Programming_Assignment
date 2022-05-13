4960daa1638db1384fcb826e9340e39831194706
KeertanaG <keertana.govte@yesgnome.com>
2022-05-13 11:10:15 +0530

    Checking Timer Functionality.

---
+++ Assets/Chapter7/Timer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float totalSeconds = 0;
+    float elapsedSeconds = 0;
+    bool running = false;
+    bool started = false;
+    public float Duration
+    {
+        set
+        {
+            if (!running)
+            {
+                totalSeconds = value;
+            }
+        }
+    }
+    public bool Finished
+    {
+        get { return started && !running; }
+    }
+    public bool Running
+    {
+        get { return running; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running)
+        {
+            elapsedSeconds += Time.deltaTime;
+            if (elapsedSeconds >= totalSeconds)
+            {
+                running = false;
+            }
+        }
+    }
+
+    public void Run()
+    {
+        // only run with valid duration
+        if (totalSeconds > 0)
+        {
+            started = true;
+            running = true;
+        }
+    }
+}
