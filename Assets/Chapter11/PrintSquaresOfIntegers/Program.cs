d2a986f1a230e75304b7b40c719a5acc28a878e7
KeertanaG <keertana.govte@yesgnome.com>
2022-05-13 11:27:38 +0530

    Printing squares of integers using loops.

---
+++ Assets/Chapter11/PrintSquaresOfIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintSquaresOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of squares to print: ");
+            int n = int.Parse(Console.ReadLine());
+            // print the squares
+            int i = 1;
+            while (i <= n)
+            {
+                Console.WriteLine("The square of {0} is {1}", i, i * i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
